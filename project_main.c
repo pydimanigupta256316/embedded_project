f38a86020546b2e0920f68a5507b94ffc55e9fd4
pydimanigupta256316 <pydimanigupta123@gmail.com>
2021-04-24 12:55:49 +0530

    chnages

--- project_main.c
+++ project_main.c
@@ -1,6 +1,6 @@
 /**
  * @file project_main.c
- * @author Bharath.G ()
+ * @author manigupta ()
  * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
  * @version 0.1
  * @date 2021-04-23
@@ -13,41 +13,33 @@
 #include "user_utils.h"
 #include "blinky.h"
 
-/**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
-void peripheral_init(void)
-{
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
-}
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
-/**
- * @brief Main function where the code execution starts
- * 
- * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
- */
 int main(void)
-{
-	/* Initialize Peripherals */
-	peripheral_init();
+ {
+     DDRB|=(1<<PB0); //set B=1
+     DDRD &=~(1<<PD0);
+     PORTD|=(1<<PD0);
+     DDRD &=~(1<<PD1);
+     PORTD|=(1<<PD1);
 
-	for(;;)
-	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
-	}
-	return 0;
+    while(1)
+      {if(!(PIND&(1<<PD0)))
+          {if(!(PIND&(1<<PD1)))
+           {PORTB|=(1<<PB0);
+           delay_ms(20);
+           }
+           else{
+        PORTB&=~(1<<PB0);
+        delay_ms(20);
+
+         }
+          }
+
+        else
+        {PORTB&=~(1<<PB0);
+        delay_ms(20);
+
+    }
+    }
+    
+return 0;
 }
